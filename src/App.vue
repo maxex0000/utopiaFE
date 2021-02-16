cc777b0b6faaff081daf61999f52f63da3767595
Syun <cj654vmp@gmail.com>
2021-02-16 18:25:47 +0800

    update 0216

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  created() {
+    this.$http.get('https://randomuser.me/api/')
+        .then((response) => {
+            console.log(response.data)
+        });
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: gray;
+}
+</style>
